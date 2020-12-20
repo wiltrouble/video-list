7a4bd53a7bf2f6a9e3ac55281f4ef8682328df61
wiltrouble <wiltrouble@pop-os.localdomain>
2020-12-20 10:00:25 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import HelloWorld from './components/HelloWorld.jsx'
+
+ReactDOM.render(
+    <HelloWorld></HelloWorld>, document.getElementById("app")
+);
