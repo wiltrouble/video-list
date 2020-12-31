25da2e019078f58efdb1a0097cc0cb1563e322d2
Wiltrouble <wiltrouble@gmail.com>
2020-12-31 12:21:47 -0400

    Not found component added

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from '../containers/Home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
+import NotFound from '../containers/NotFound'
 
 const App = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />`
+            <Route exact path="/register" component={Register} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>
 )
